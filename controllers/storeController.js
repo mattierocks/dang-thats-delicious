e084a7a5f42b6dd8a9ad85950dbb6c1499e4c011
mattierocks <mattierocks@gmail.com>
2018-02-15 16:00:43 -0500

    start bulding out search backend

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -99,3 +99,17 @@ exports.getStoresByTag = async(req, res) => {
 
     res.render('tag', { tags, title: 'Tags', tag, stores });
 };
+
+exports.searchStores = async(req, res) => {
+    const stores = await Store.find({
+            $text: {
+                $search: req.query.q
+            }
+        }, {
+            score: { $meta: 'textScore' }
+        })
+        .sort({
+            score: { $meta: 'textScore' }
+        })
+        .limit(5);
+};
--- routes/index.js
+++ routes/index.js
@@ -46,4 +46,9 @@ router.post('/account/forgot', catchErrors(authController.forgot));
 router.get('/account/reset/:token', catchErrors(authController.reset));
 router.post('/account/reset/:token', authController.confirmedPasswords, catchErrors(authController.update));
 
+/*
+  API
+*/
+router.get('/api/search', catchErrors(storeController.searchStores));
+
 module.exports = router;

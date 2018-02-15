95ebcfeb92a4f5c00d1a7c9dbdc9cb70073791d5
mattierocks <mattierocks@gmail.com>
2018-02-15 12:40:12 -0500

    add login requirement to add store

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,19 @@ exports.login = passport.authenticate('local', {
     successRedirect: '/',
     successFlash: 'You are now logged in!'
 });
+
+exports.logout = (req, res) => {
+    req.logout();
+    req.flash('success', 'You are now logged out!');
+    res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+    // first check if user is authenticated
+    if (req.isAuthenticated()) {
+        next(); // carry on, they are logged in
+        return;
+    }
+    req.flash('error', 'You must be logged in to do that!');
+    res.redirect('/login');
+}
